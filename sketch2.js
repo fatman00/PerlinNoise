12467da094a9638cc0f93b801d07bddfe9154d59
fatman00 <fatman00hot@hotmail.com>
2018-02-03 04:20:08 -0300

    Noiseval with commend

--- sketch2.js
+++ sketch2.js
@@ -3,6 +3,7 @@ var scl = 20;
 var w = 500;
 var h = 500;
 var noisescl= 0.15;
+var noiseval = 0;
 
 var flying = 0;
 
@@ -16,6 +17,7 @@ function setup() {
 
 function draw() {
   background(100);
+  // noiseSeed(noiseval);
   stroke(0);
   for(var i = 0; i < cols; i++) {
     for(var j = 0;j<rows;j++) {
@@ -23,10 +25,9 @@ function draw() {
       // fill(map(noise(i*noisescl,j*noisescl),0,1,0,255));
       // rect(i*scl,j*scl,scl-1,scl-1);
       var dir = map(noise(i*noisescl,j*noisescl),0,1,0,TWO_PI);
+      // var dirvec = new vec
       line(i*scl, j*scl,i*scl+cos(dir)*scl,j*scl+sin(dir)*scl);
     }
   }
-  // rect(50,50,80,80);
-
   noLoop();
 }
