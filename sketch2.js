f93b8288e48d1b6b8666fa7f347643e0cfec9c1a
fatman00 <fatman00hot@hotmail.com>
2018-02-03 04:05:17 -0300

    Drawing manual vectors now

--- sketch2.js
+++ sketch2.js
@@ -1,5 +1,5 @@
 var cols, rows;
-var scl = 10;
+var scl = 20;
 var w = 500;
 var h = 500;
 var noisescl= 0.15;
@@ -20,8 +20,10 @@ function draw() {
   for(var i = 0; i < cols; i++) {
     for(var j = 0;j<rows;j++) {
       // Gennerate Perlin Noise from the X,Y coordinates, scale it and map it to BW color scale
-      fill(map(noise(i*noisescl,j*noisescl),0,1,0,255));
-      rect(i*scl,j*scl,scl-1,scl-1);
+      // fill(map(noise(i*noisescl,j*noisescl),0,1,0,255));
+      // rect(i*scl,j*scl,scl-1,scl-1);
+      var dir = map(noise(i*noisescl,j*noisescl),0,1,0,PI);
+      line(i*scl, j*scl,i*scl+cos(dir)*scl,j*scl+sin(dir)*scl);
     }
   }
   // rect(50,50,80,80);
