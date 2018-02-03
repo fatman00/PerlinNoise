29373b23b3a0dd3b9034763b37f658e3451f04e1
fatman00 <fatman00hot@hotmail.com>
2018-02-03 03:58:38 -0300

    Code Cleanup

--- sketch2.js
+++ sketch2.js
@@ -14,38 +14,9 @@ function setup() {
   rows = h/ scl;
 }
 
-
-
-// function draw() {
-//   background(50);
-//   stroke(255);
-//   noFill();
-//   beginShape();
-//   var xoff=0;
-//   for (var x = 0; x < width;x++){
-//     var y = noise(xoff) * height;
-//     vertex(x,y);
-//     xoff+=0.02
-//   }
-//   endShape();
-// }
-// function draw() {
-//   if(mouseIsPressed) {
-//     fill(0);
-//   } else {
-//     fill(255);
-//   }
-//   ellipse(mouseX, mouseY, 80,80);
-//
-// }
-
 function draw() {
-  // for(var i = 0; i < terrain.length;i++) {
-  //
-  // }
   background(100);
   stroke(0);
-  // fill(255);
   for(var i = 0; i < cols; i++) {
     for(var j = 0;j<rows;j++) {
       // Gennerate Perlin Noise from the X,Y coordinates, scale it and map it to BW color scale
