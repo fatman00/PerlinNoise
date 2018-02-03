5c0cd7f2f37a58f83b2cb942caada16138c376d5
fatman00 <fatman00hot@hotmail.com>
2018-02-03 05:04:58 -0300

    Now using a vector and translate/rotate for the line

--- sketch2.js
+++ sketch2.js
@@ -4,6 +4,7 @@ var w = 500;
 var h = 500;
 var noisescl= 0.15;
 var noiseval = 0;
+var noiseinc = 0;
 
 var flying = 0;
 
@@ -22,12 +23,18 @@ function draw() {
   for(var i = 0; i < cols; i++) {
     for(var j = 0;j<rows;j++) {
       // Gennerate Perlin Noise from the X,Y coordinates, scale it and map it to BW color scale
-      // fill(map(noise(i*noisescl,j*noisescl),0,1,0,255));
-      // rect(i*scl,j*scl,scl-1,scl-1);
       var dir = map(noise(i*noisescl,j*noisescl),0,1,0,TWO_PI);
-      // var dirvec = new vec
-      line(i*scl, j*scl,i*scl+cos(dir)*scl,j*scl+sin(dir)*scl);
+      var dirvec = p5.Vector.fromAngle(dir);
+      //line(i*scl, j*scl,i*scl+cos(dir)*scl,j*scl+sin(dir)*scl);
+      stroke(0);
+      push();
+      translate(i*scl,j*scl);
+      rotate(dirvec.heading());
+      line(0,0,scl,0);
+
+      pop();
     }
   }
   noLoop();
+  fr.html(floot(frameRate()));
 }
