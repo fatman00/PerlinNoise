0abf515191005911e34a20076cfd43b5c632c3f2
fatman00 <fatman00hot@hotmail.com>
2018-02-03 04:29:50 -0300

    Random pixels on the screen

--- sketch2.js
+++ sketch2.js
@@ -13,21 +13,21 @@ function setup() {
   createCanvas(w, h);
   cols = w/ scl;
   rows = h/ scl;
+  pixelDensity(1);
 }
 
 function draw() {
-  background(100);
-  // noiseSeed(noiseval);
-  stroke(0);
-  for(var i = 0; i < cols; i++) {
-    for(var j = 0;j<rows;j++) {
-      // Gennerate Perlin Noise from the X,Y coordinates, scale it and map it to BW color scale
-      // fill(map(noise(i*noisescl,j*noisescl),0,1,0,255));
-      // rect(i*scl,j*scl,scl-1,scl-1);
-      var dir = map(noise(i*noisescl,j*noisescl),0,1,0,TWO_PI);
-      // var dirvec = new vec
-      line(i*scl, j*scl,i*scl+cos(dir)*scl,j*scl+sin(dir)*scl);
+loadPixels();
+  for(var i = 0; i < width; i++) {
+    for(var j = 0;j<height;j++) {
+      var index = (i+j*width)*4;
+      var r = random(255);
+      pixels[index+0] = r;
+      pixels[index+1] = r;
+      pixels[index+2] = r;
+      pixels[index+3] = 255;
     }
   }
+  updatePixels();
   noLoop();
 }
