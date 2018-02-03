b28ecd539ad22bba09e5328186e6564fa4bb0b75
fatman00 <fatman00hot@hotmail.com>
2018-02-03 03:31:44 -0300

    Basic draw of a circle at mouse position

---
+++ sketch2.js
@@ -0,0 +1,58 @@
+var cols, rows;
+var scl = 10;
+var w = 500;
+var h = 500;
+
+var flying = 0;
+
+var terrain = [];
+
+function setup() {
+  createCanvas(w, h);
+  cols = w/ scl;
+  rows = h/ scl;
+}
+
+
+
+// function draw() {
+//   background(50);
+//   stroke(255);
+//   noFill();
+//   beginShape();
+//   var xoff=0;
+//   for (var x = 0; x < width;x++){
+//     var y = noise(xoff) * height;
+//     vertex(x,y);
+//     xoff+=0.02
+//   }
+//   endShape();
+// }
+function draw() {
+  if(mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80,80);
+
+}
+
+// function draw() {
+//   // for(var i = 0; i < terrain.length;i++) {
+//   //
+//   // }
+//   background(100);
+//   stroke(0);
+//   // fill(255);
+//   for(var i = 0; i < cols; i++) {
+//     for(var j = 0;j<rows;j++) {
+//       // Gennerate Perlin Noise from the X,Y coordinates, scale it and map it to BW color scale
+//       fill(map(noise(i,j)*0.3,0,1,0,255));
+//       rect(i*scl,j*scl,scl-1,scl-1);
+//     }
+//   }
+//   // rect(50,50,80,80);
+//
+//   noLoop();
+// }
