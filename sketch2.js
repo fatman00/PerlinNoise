5b2ae1b37afd965f5c6c5d45de7836f876a546d8
fatman00 <fatman00hot@hotmail.com>
2018-02-03 04:36:30 -0300

    Using noise for a 1d pixel array

--- sketch2.js
+++ sketch2.js
@@ -1,9 +1,10 @@
 var cols, rows;
 var scl = 20;
 var w = 500;
-var h = 500;
+var h = 400;
 var noisescl= 0.15;
 var noiseval = 0;
+var xoff;
 
 var flying = 0;
 
@@ -17,15 +18,18 @@ function setup() {
 }
 
 function draw() {
+  var xoff=0;
 loadPixels();
   for(var i = 0; i < width; i++) {
     for(var j = 0;j<height;j++) {
       var index = (i+j*width)*4;
-      var r = random(255);
+      // var r = random(255);
+      var r = noise(xoff)*255;
       pixels[index+0] = r;
       pixels[index+1] = r;
       pixels[index+2] = r;
       pixels[index+3] = 255;
+      xoff+=0.01;
     }
   }
   updatePixels();
