df7d940a15f82b53b9f605c2754f6b5ac138c63a
fatman00 <fatman00hot@hotmail.com>
2018-02-03 04:10:53 -0300

    fixed radian TWO-PI

--- sketch2.js
+++ sketch2.js
@@ -22,7 +22,7 @@ function draw() {
       // Gennerate Perlin Noise from the X,Y coordinates, scale it and map it to BW color scale
       // fill(map(noise(i*noisescl,j*noisescl),0,1,0,255));
       // rect(i*scl,j*scl,scl-1,scl-1);
-      var dir = map(noise(i*noisescl,j*noisescl),0,1,0,PI);
+      var dir = map(noise(i*noisescl,j*noisescl),0,1,0,TWO_PI);
       line(i*scl, j*scl,i*scl+cos(dir)*scl,j*scl+sin(dir)*scl);
     }
   }
